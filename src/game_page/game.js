04fe71d88eb24ce1c907547d51e6bed7e050c5d2
Abid-Chowdhury <mr.abid2004@gmail.com>
2023-10-30 23:33:08 -0400

    Added js

---
+++ src/game_page/game.js
@@ -0,0 +1,30 @@
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))]
+}
+
+var first = document.getElementById('first')
+var second = document.getElementById('second')
+var operation = document.getElementById('operation')
+
+function generate_Random_Nums() {
+    first_Num = Math.floor(Math.random() * 10)
+    second_Num = Math.floor(Math.random() * 10)
+    random_Operation = ['+', '-', '*', '/'].random()
+    return [first_Num, second_Num, random_Operation]
+}
+
+var random_Numbers = generate_Random_Nums()
+
+function update_Numbers(random_Numbers) {
+    first.innerText = random_Numbers[0]
+    second.innerText = random_Numbers[1]
+    operation.innerText = random_Numbers[2]
+}
+
+function answer(random_Numbers) {
+    return eval(random_Numbers[0] + random_Numbers[2] + random_Numbers[1])
+}
+
+console.log(random_Numbers)
+update_Numbers(random_Numbers)
+answer(random_Numbers)
