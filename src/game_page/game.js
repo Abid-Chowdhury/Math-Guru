87245f2c04b7445c51596c3f376c833d1e709ab5
Abid-Chowdhury <mr.abid2004@gmail.com>
2024-01-06 06:31:56 -0500

    Added generate_Random_Oper function

--- src/game_page/game.js
+++ src/game_page/game.js
@@ -9,22 +9,27 @@ var operation = document.getElementById('operation')
 function generate_Random_Nums() {
     first_Num = Math.floor(Math.random() * 10)
     second_Num = Math.floor(Math.random() * 10)
-    random_Operation = ['+', '-', '*', '/'].random()
-    return [first_Num, second_Num, random_Operation]
+    return [first_Num, second_Num]
+}
+
+function generate_Random_Oper() {
+    random_Oper = ['+', '-', '*', '/'].random()
+    return random_Oper
 }
 
 var random_Numbers = generate_Random_Nums()
+var random_Operation = generate_Random_Oper()
 
-function update_Numbers(random_Numbers) {
+function update_Numbers(random_Numbers, random_Operation) {
     first.innerText = random_Numbers[0]
     second.innerText = random_Numbers[1]
-    operation.innerText = random_Numbers[2]
+    operation.innerText = random_Operation
 }
 
-function answer(random_Numbers) {
-    answer = eval(random_Numbers[0] + random_Numbers[2] + random_Numbers[1])
+function answer(random_Numbers, random_Operation) {
+    answer = eval(random_Numbers[0] + random_Operation + random_Numbers[1])
     return answer
 }
 
-update_Numbers(random_Numbers)
-answer(random_Numbers)
+update_Numbers(random_Numbers, random_Operation)
+console.log(answer(random_Numbers, random_Operation))
