ac0b87c057788ac5f0f10aac88eedb0d09b679f1
Abid-Chowdhury <mr.abid2004@gmail.com>
2024-01-06 06:41:31 -0500

    added difficulty variable

--- src/game_page/game.js
+++ src/game_page/game.js
@@ -1,31 +1,41 @@
+// get current difficulty
+var difficulty = (window.location.search).replace('?', '')
+
+// function to get random num & operation
 Array.prototype.random = function () {
     return this[Math.floor((Math.random()*this.length))]
 }
 
+// set vars for nums & operations
 var first = document.getElementById('first')
 var second = document.getElementById('second')
 var operation = document.getElementById('operation')
 
-function generate_Random_Nums() {
+// generates random numbers based on difficulty
+function generate_Random_Nums(difficulty) {
+    console.log(difficulty)
     first_Num = Math.floor(Math.random() * 10)
     second_Num = Math.floor(Math.random() * 10)
     return [first_Num, second_Num]
 }
 
+// generates random operation
 function generate_Random_Oper() {
     random_Oper = ['+', '-', '*', '/'].random()
     return random_Oper
 }
 
-var random_Numbers = generate_Random_Nums()
+var random_Numbers = generate_Random_Nums(difficulty)
 var random_Operation = generate_Random_Oper()
 
+// update numbers of page to random numbers
 function update_Numbers(random_Numbers, random_Operation) {
     first.innerText = random_Numbers[0]
     second.innerText = random_Numbers[1]
     operation.innerText = random_Operation
 }
 
+// correct answer to question
 function answer(random_Numbers, random_Operation) {
     answer = eval(random_Numbers[0] + random_Operation + random_Numbers[1])
     return answer
