03aa8d0a8fd1a1f1de03cfd996bbf8c67fc66a03
Abid-Chowdhury <mr.abid2004@gmail.com>
2024-01-08 06:17:18 -0500

    Added hover effect to footer

--- src/home_page/script.js
+++ src/home_page/script.js
@@ -5,3 +5,13 @@ function redirect(difficulty) {
 function redirect_To_Home_Page() {
     window.location.href = "index.html"
 }
+
+var image = document.getElementById('github-image')
+
+function hover() {
+    image.setAttribute('src', "../images/github_active.svg")
+}
+
+function unhover() {
+  image.setAttribute('src', "../images/github.svg")
+}
